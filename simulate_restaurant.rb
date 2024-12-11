88302a3a40c3b133c53e7acb0eeebf5bf1616c7e
Jesus Medina <jesus.medina05@gmail.com>
2024-12-11 12:41:16 -0300

    modificate script for get first restaurant of the bd

--- simulate_restaurant.rb
+++ simulate_restaurant.rb
@@ -2,11 +2,26 @@ require 'net/http'
 require 'json'
 
 API_BASE_URL = 'http://localhost:3000/api/devices/change_status'
-RESTAURANT_ID = 1
 STATUS_OPTIONS = %w[operativo warning Problemas]
 
-def fetch_devices
-  uri = URI("http://localhost:3000/api/restaurants/#{RESTAURANT_ID}/devices")
+def fetch_first_restaurant
+  uri = URI('http://localhost:3000/api/restaurants')
+  response = Net::HTTP.get_response(uri)
+
+  if response.is_a?(Net::HTTPSuccess)
+    restaurants = JSON.parse(response.body)
+    return restaurants.first['id'] if restaurants.any?
+
+    puts "[#{Time.now}] No se encontraron restaurantes."
+    nil
+  else
+    puts "[#{Time.now}] Error al obtener restaurantes: #{response.code} - #{response.body}"
+    nil
+  end
+end
+
+def fetch_devices(restaurant_id)
+  uri = URI("http://localhost:3000/api/restaurants/#{restaurant_id}/devices")
   response = Net::HTTP.get_response(uri)
 
   if response.is_a?(Net::HTTPSuccess)
@@ -18,17 +33,17 @@ def fetch_devices
 end
 
 def random_status(current_status)
-  new_status = STATUS_OPTIONS.sample
+  STATUS_OPTIONS.sample
 end
 
-def send_to_api(device_id, new_status)
+def send_to_api(device_id, restaurant_id, new_status)
   uri = URI(API_BASE_URL)
 
   request = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
   request.body = {
     device: {
       id: device_id,
-      restaurant_id: RESTAURANT_ID,
+      restaurant_id: restaurant_id,
       status: new_status
     }
   }.to_json
@@ -52,15 +67,18 @@ def handle_response(response)
 end
 
 def simulate_devices
+  restaurant_id = fetch_first_restaurant
+  return unless restaurant_id
+
   loop do
-    device_ids = fetch_devices
+    device_ids = fetch_devices(restaurant_id)
     if device_ids.empty?
-      puts "[#{Time.now}] No se encontraron dispositivos para el restaurante #{RESTAURANT_ID}."
+      puts "[#{Time.now}] No se encontraron dispositivos para el restaurante #{restaurant_id}."
     else
       device_ids.each do |device_id|
         new_status = random_status('operativo')
         puts "[#{Time.now}] Enviando actualización: Dispositivo #{device_id}, Estado: #{new_status}"
-        send_to_api(device_id, new_status)
+        send_to_api(device_id, restaurant_id, new_status)
       end
     end
     sleep(rand(10..15))
